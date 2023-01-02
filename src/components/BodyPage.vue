909b26436e90161400079a7101479d6e511fc344
SuelenLaffinWarren <suelen.laffin@warren.com.br>
2023-01-02 14:22:53 -0300

    fix: correcting the spacing

--- src/components/BodyPage.vue
+++ src/components/BodyPage.vue
@@ -1,29 +1,42 @@
 <template>
     <div id="div-principal">
-        <div id="div-body">
-            <img src="/img/warren-family.jpg" alt="" id="img-1">
-
-            <h4 id="h4-1">Est saepe quis quo sint voluptatum eum atque incidunt eum eligendi dolores sit velit rerum
-                non
-                temporibus ipsam ad ipsum voluptatem. Est rerum debitis eos dicta dolorem et enim iste quo
-                repudiandae suscipit. <br><br> Et doloremque nihil ea voluptatem quia et minima dolores quo rerum
-                voluptas
-                sit maiores praesentium aut cupiditate accusantium hic tempora beatae. Rem dolorem Quis qui
-                impedit
-                quam qui architecto neque ex quas ullam!</h4>
-
+        <div id="div-body1">
+            <div>
+                <img src="/img/warren-family.jpg" alt="" id="img-1">
+            </div>
+            <div id="div-text1">
+                <h2>Est saepe quis quo sint</h2>
+                <h4 id="h4-1">Est saepe quis quo sint voluptatum eum atque incidunt eum eligendi dolores sit velit rerum
+                    non
+                    temporibus ipsam ad ipsum voluptatem. Est rerum debitis eos dicta dolorem et enim iste quo
+                    repudiandae suscipit.
+                    <hr> Et doloremque nihil ea voluptatem quia et minima dolores quo rerum
+                    voluptas
+                    sit maiores praesentium aut cupiditate accusantium hic tempora beatae. Rem dolorem Quis qui
+                    impedit
+                    quam qui architecto neque ex quas ullam!
+                </h4>
+                <button>Button</button>
+            </div>
         </div>
-        <div id="div-body">
-            <h4 id="h4-2">Est saepe quis quo sint voluptatum eum atque incidunt eum eligendi dolores sit velit rerum
-                non
-                temporibus ipsam ad ipsum voluptatem. Est rerum debitis eos dicta dolorem et enim iste quo
-                repudiandae suscipit. <br><br> Et doloremque nihil ea voluptatem quia et minima dolores quo rerum
-                voluptas
-                sit maiores praesentium aut cupiditate accusantium hic tempora beatae. Rem dolorem Quis qui
-                impedit
-                quam qui architecto neque ex quas ullam!
-            </h4>
-            <img src="/img/warren-family.jpg" alt="" id="img-2">
+        <div id="div-body2">
+            <div id="div-text2">
+                <h2>Est saepe quis quo sint</h2>
+                <h4 id="h4-2">Est saepe quis quo sint voluptatum eum atque incidunt eum eligendi dolores sit velit rerum
+                    non
+                    temporibus ipsam ad ipsum voluptatem. Est rerum debitis eos dicta dolorem et enim iste quo
+                    repudiandae suscipit.
+                    <hr> Et doloremque nihil ea voluptatem quia et minima dolores quo rerum
+                    voluptas
+                    sit maiores praesentium aut cupiditate accusantium hic tempora beatae. Rem dolorem Quis qui
+                    impedit
+                    quam qui architecto neque ex quas ullam!
+                </h4>
+                <button>Button</button>
+            </div>
+            <div>
+                <img src="/img/warren-family.jpg" alt="" id="img-2">
+            </div>
         </div>
 
     </div>
@@ -36,11 +49,49 @@ export default {
 </script>
 
 <style scoped>
+hr {
+    background-color: rgb(244, 8, 122);
+}
+
+button {
+    background-color: black;
+    color: white;
+    border: none;
+    border-radius: 15px;
+    font-size: 14px;
+    width: 150px;
+    height: 40px;
+    font-weight: bold;
+
+}
+
+#div-body1 {
+    margin: 30px;
+    padding-left: 30px;
+}
+
+#div-body2 {
+    margin: 30px;
+    padding-right: 30px;
+}
+
+#div-text1 {
+    flex-direction: column;
+    padding-left: 50px;
+    padding-right: 30px;
+}
+
+#div-text2 {
+    flex-direction: column;
+    padding-right: 50px;
+    padding-left: 30px;
+    text-align: start;
+}
+
 #img-1 {
     justify-items: flex-start;
     width: 370px;
     height: 400px;
-    margin-left: 100px;
     border-radius: 15px;
 }
 
@@ -48,15 +99,11 @@ export default {
     justify-items: flex-start;
     width: 370px;
     height: 400px;
-    margin-right: 150px;
     border-radius: 15px;
-
 }
 
 #h4-1 {
     text-align: start;
-    margin-right: 150px;
-    margin-left: 300px;
     font-family: serif;
     color: black;
     font-weight: bold;
@@ -64,8 +111,6 @@ export default {
 
 #h4-2 {
     text-align: start;
-    margin-right: 300px;
-    margin-left: 100px;
     font-family: serif;
     color: black;
     font-weight: bold;
@@ -73,7 +118,6 @@ export default {
 
 div {
     text-align: start;
-    margin-left: 10px;
     flex-direction: row;
     margin-top: 20px;
     display: flex;
