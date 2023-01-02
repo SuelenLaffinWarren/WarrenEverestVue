a604bb3119a78ed319c20d6f23a84ac07a760c8a
SuelenLaffinWarren <suelen.laffin@warren.com.br>
2023-01-02 11:43:11 -0300

    fix: changing banner texts

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,14 +3,14 @@
         <div>
             <form action="">
                 <div>
-                    <h2 id="h2-banner">Lorem ipsum dolor sit amet. Nam voluptas quidem id rerum eius.
+                    <h2 id="h2-banner">Invista em viver.
                     </h2>
                 </div>
                 <div>
-                    <p>Lorem ipsum dolor sit amet. Eos culpa error et illum quidem ut sint</p>
+                    <p>Investir toma tempo, mas não precisa ser o seu. Conte com nossos especialistas.</p>
                 </div>
                 <div>
-                    <button id="btn">BUTTON</button>
+                    <button id="btn">Comece agora</button>
                 </div>
             </form>
         </div>
@@ -44,16 +44,8 @@ export default {
 
 }
 
-#main-banner h1 {
-    color: white;
-    text-align: center;
-    font-size: 50px;
-    background-color: #222;
-    padding: 20px 40px;
-}
-
 #h2-banner {
-    font-size: 35px;
+    font-size: 55px;
     color: rgb(238, 228, 228);
     text-align: left;
     margin-left: 200px;
@@ -70,7 +62,7 @@ p {
     color: white;
     font-style: italic;
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    font-size: 12px;
+    font-size: 22px;
     margin-top: 10px;
     margin-left: 200px;
     margin-right: 500px;
@@ -81,10 +73,11 @@ button {
     color: black;
     border: none;
     border-radius: 15px;
-    font-size: 12px;
-    width: 80px;
-    height: 30px;
+    font-size: 14px;
+    width: 150px;
+    height: 40px;
     margin-right: 200px;
     margin-left: 200px;
+    font-weight: bold;
 }
 </style>
